fcae6993f98f8c1b1b52cd3614e44dfc0669ecb2
davalapar <davalapar@gmail.com>
2019-10-18 15:36:23 +0800

    yeh

--- src/haversine.js
+++ src/haversine.js
@@ -13,11 +13,13 @@ const earthRadius = 6371e3;
  * @returns {number} meters
  */
 const haversine = (lat1, lon1, lat2, lon2) => {
+  // first latitude in radians
   const lat1r = lat1 * degreesToRadiansMultiplier;
+  // second latitude in radians
   const lat2r = lat2 * degreesToRadiansMultiplier;
-  // latitude delta:
+  // delta between first and second latitude:
   const latdr = (lat2 - lat1) * degreesToRadiansMultiplier;
-  // longitude delta:
+  // delta between first and second longitude:
   const londr = (lon2 - lon1) * degreesToRadiansMultiplier;
   // square of half the chord length between the points
   const x = Math.sin(latdr / 2) * Math.sin(latdr / 2) + Math.cos(lat1r) * Math.cos(lat2r) * Math.sin(londr / 2) * Math.sin(londr / 2);
