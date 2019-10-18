f845eff61449b3b2bc278f67dce29a371770aecc
davalapar <davalapar@gmail.com>
2019-10-18 16:26:25 +0800

    optimize haversine

--- src/haversine.js
+++ src/haversine.js
@@ -17,12 +17,12 @@ const haversine = (lat1, lon1, lat2, lon2) => {
   const lat1r = lat1 * degreesToRadiansMultiplier;
   // second latitude in radians
   const lat2r = lat2 * degreesToRadiansMultiplier;
-  // delta between first and second latitude:
-  const latdr = (lat2 - lat1) * degreesToRadiansMultiplier;
-  // delta between first and second longitude:
-  const londr = (lon2 - lon1) * degreesToRadiansMultiplier;
+  // half of delta between first and second latitude:
+  const latdh = ((lat2 - lat1) * degreesToRadiansMultiplier) / 2;
+  // half of delta between first and second longitude:
+  const londh = ((lon2 - lon1) * degreesToRadiansMultiplier) / 2;
   // square of half the chord length between the points
-  const x = Math.sin(latdr / 2) * Math.sin(latdr / 2) + Math.cos(lat1r) * Math.cos(lat2r) * Math.sin(londr / 2) * Math.sin(londr / 2);
+  const x = Math.sin(latdh) * Math.sin(latdh) + Math.cos(lat1r) * Math.cos(lat2r) * Math.sin(londh) * Math.sin(londh);
   // earth radius * angular distance in radians:
   return earthRadius * (2 * Math.atan2(Math.sqrt(x), Math.sqrt(1 - x)));
 };
