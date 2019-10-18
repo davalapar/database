a9339717d8f1b6a426e638ae100fbac641377f57
davalapar <davalapar@gmail.com>
2019-10-18 15:31:59 +0800

    haversine optimize

--- src/haversine.js
+++ src/haversine.js
@@ -1,25 +1,28 @@
 
-const radians = (degrees) => degrees * (Math.PI / 180);
+// const radians = (degrees) => degrees * (Math.PI / 180);
 
+const degreesToRadiansMultiplier = Math.PI / 180;
 const earthRadius = 6371e3;
 
 /**
  *  Haversine: https://www.movable-type.co.uk/scripts/latlong.html
- * @param {number} lat1
- * @param {number} lon1
- * @param {number} lat2
- * @param {number} lon2
- * @returns {number} Kilometers
+ * @param {number} lat1 first coordinate latitude in degrees
+ * @param {number} lon1 first coordinate longitude in degrees
+ * @param {number} lat2 second coordinate latitude in degrees
+ * @param {number} lon2 second coordinate longitude in degrees
+ * @returns {number} meters
  */
 const haversine = (lat1, lon1, lat2, lon2) => {
-  const lat1r = radians(lat1);
-  const lat2r = radians(lat2);
-  const latdr = radians(lat2 - lat1);
-  const londr = radians(lon2 - lon1);
-  const a = Math.sin(latdr / 2) * Math.sin(latdr / 2) + Math.cos(lat1r) * Math.cos(lat2r) * Math.sin(londr / 2) * Math.sin(londr / 2);
-  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-  const distance = earthRadius * c;
-  return distance;
+  const lat1r = lat1 * degreesToRadiansMultiplier;
+  const lat2r = lat2 * degreesToRadiansMultiplier;
+  // latitude delta:
+  const latdr = (lat2 - lat1) * degreesToRadiansMultiplier;
+  // longitude delta:
+  const londr = (lon2 - lon1) * degreesToRadiansMultiplier;
+  // square of half the chord length between the points
+  const x = Math.sin(latdr / 2) * Math.sin(latdr / 2) + Math.cos(lat1r) * Math.cos(lat2r) * Math.sin(londr / 2) * Math.sin(londr / 2);
+  // earth radius * angular distance in radians:
+  return earthRadius * (2 * Math.atan2(Math.sqrt(x), Math.sqrt(1 - x)));
 };
 
 module.exports = haversine;
